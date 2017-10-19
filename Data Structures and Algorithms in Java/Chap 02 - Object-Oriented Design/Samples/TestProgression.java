e30e2e81e62e34f098ee83840ff40af1c83b72f7
Driveron <driverland@163.com>
2017-10-19 10:56:04 +0800

    #37: Add Exercise 2.6

--- Data Structures and Algorithms in Java/Chap 02 - Object-Oriented Design/Samples/TestProgression.java
+++ Data Structures and Algorithms in Java/Chap 02 - Object-Oriented Design/Samples/TestProgression.java
@@ -5,37 +5,7 @@
 
 public class TestProgression {
     public static void main(String[] args) {
-        Progression prog;  // use the attribute of polymorphism
-
-        // test arithmetic progression
-        System.out.print("Arithmic progression with default increment: ");
-        prog = new ArithmeticProgression();
-        prog.printProgression(10);
-
-        System.out.print("Arithmetic progression with increment 7: ");
-        prog = new ArithmeticProgression(7);
-        prog.printProgression(10);
-
-        System.out.print("Arithmetic progression start with 2: ");
-        prog = new ArithmeticProgression(7, 2);
-        prog.printProgression(10);
-
-        // test geometric progression
-        System.out.print("Geometric progression with default base: ");
-        prog = new GeometricProgression();
-        prog.printProgression(10);
-
-        System.out.print("Geometric progression with base 4: ");
-        prog = new GeometricProgression(4);
-        prog.printProgression(10);
-
-        // test Fibonacci progression
-        System.out.print("Fibonacci progression with default start values: ");
-        prog = new FibonacciProgression();
-        prog.printProgression(10);
-
-        System.out.print("Fibonacci progression start with 23 and 27: ");
-        prog = new FibonacciProgression(23, 27);
-        prog.printProgression(7);
+        Progression prog = new FibonacciProgression(2, 2);
+        prog.printProgression(8);  // prints eight numbers from the start
     }
 }
