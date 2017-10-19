f5dd86f29f39cc07e009ea0cbf7e758ce75f4381
Driveron <driverland@163.com>
2017-10-19 11:11:34 +0800

    #39: Add Exercise 2.7

---
+++ Data Structures and Algorithms in Java/Chap 02 - Object-Oriented Design/Exercises/2.7/TestProgression.java
@@ -0,0 +1,11 @@
+/**
+ * This file is aimed to test the hierarchy of 
+ * inheritance of the Progression classes
+ */
+
+public class TestProgression {
+    public static void main(String[] args) {
+        Progression prog = new ArithmeticProgression(128);
+        prog.printProgression(16777217);  
+    }
+}
