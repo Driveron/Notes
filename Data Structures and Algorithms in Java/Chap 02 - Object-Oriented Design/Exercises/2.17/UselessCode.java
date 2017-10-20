a7fa982bf2db4959f7c781273aa528a2341b6266
Driveron <driverland@163.com>
2017-10-20 13:31:18 +0800

    #43: Add Exercise 2.17 -- To write some useless codes

---
+++ Data Structures and Algorithms in Java/Chap 02 - Object-Oriented Design/Exercises/2.17/UselessCode.java
@@ -0,0 +1,15 @@
+
+public class UselessCode {
+    public static void main(String[] args) {
+        int x = 0;
+        if (x > 0) {
+            System.out.println("Useless");
+        }
+        else if (x == 0) {
+            System.out.println("Useful");
+        }
+        else {
+            System.out.println("Useless");
+        }
+    }
+}
