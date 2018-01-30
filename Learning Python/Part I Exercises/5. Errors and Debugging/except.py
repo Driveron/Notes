12a101ecf198db4f26675f8397e044da86718bd8
Driveron <Driverland@163.com>
2018-01-30 23:02:49 +0800

    #70: Added new note -- Learning Python with its 'Part I Exercises'

---
+++ Learning Python/Part I Exercises/5. Errors and Debugging/except.py
@@ -0,0 +1,3 @@
+print(2 ** 100)
+print(1 / 0)
+Undefined
---
+++ Learning Python/Part I Exercises/6. Breaks and cycles/warn.py
@@ -0,0 +1,3 @@
+L = [1, 2]                  # Make a 2-item list
+L.append(L)                 # Append L as a single item to itself
+print(L)                    # Print L: a cyclic/circular object
